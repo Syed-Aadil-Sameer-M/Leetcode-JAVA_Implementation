81416c523060668c2429dc01e729c1a976e793b9
Syed Aadil Sameer Moosa <aadilsameer22@gmail.com>
2026-04-08 22:53:11 +0530

    Time: 74 ms (5.33%), Space: 48 MB (41.33%) - LeetHub

---
+++ 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int xorAfterQueries(int[] nums, int[][] queries) {
+        final int MOD = 1000000007;
+        int n = nums.length;
+        long xorResult = 0;
+
+        for (int num : nums) {
+            xorResult ^= num;
+        }
+
+        for (int[] query : queries) {
+            int l = query[0];
+            int r = query[1];
+            int k = query[2];
+            int v = query[3];
+
+            for (int idx = l; idx <= r; idx += k) {
+                xorResult ^= nums[idx]; 
+                nums[idx] = (int)((1L * nums[idx] * v) % MOD); 
+                xorResult ^= nums[idx];
+            }
+        }
+
+        return (int) xorResult;
+    }
+}
