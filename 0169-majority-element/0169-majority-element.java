9d8ad919b171c6d9aec332b2d0037ac90a1e87a2
Syed Aadil Sameer Moosa <aadilsameer22@gmail.com>
2026-05-16 14:58:24 +0530

    Time: 2 ms (70.81%), Space: 55.9 MB (17.7%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+                count = 1;
+            } else if (candidate == num) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
